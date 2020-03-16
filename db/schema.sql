042b35ae8471554ad2ea4877d3942ec437039a28
Sanjay Patil <sp2004@hotmail.com>
2020-03-16 13:31:40 -0700

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+
+CREATE DATABASE burgerDb;
+USE burgerDb;
+
+CREATE TABLE burger
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devour BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
