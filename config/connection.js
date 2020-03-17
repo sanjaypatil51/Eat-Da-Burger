c24285117fa880edf5e31689785f7b682a519518
Sanjay Patil <sp2004@hotmail.com>
2020-03-16 22:38:14 -0700

    changed versions of npm pkg

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "welcome123",
-  database: "burgerDb"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "welcome123",
+//   database: "burgerDb"
+// });
 
 //heroku database
-// var connection = mysql.createConnection({
-//   host: "us-cdbr-iron-east-04.cleardb.net",
-//   port: 3306,
-//   user: "bbcf7c968de9b1",
-//   password: "13a4a4a4",
-//   database: "heroku_f5bbbb4845c5c79"
-// });
+var connection = mysql.createConnection({
+  host: "us-cdbr-iron-east-04.cleardb.net",
+  port: 3306,
+  user: "bbcf7c968de9b1",
+  password: "13a4a4a4",
+  database: "heroku_f5bbbb4845c5c79"
+});
 
 
 // Make connection.
